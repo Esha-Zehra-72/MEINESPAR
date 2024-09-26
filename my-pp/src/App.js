017e22ecaa85b3245bf5c5ec1438a4fc0b3541d6
Esha-Zehra-72 <eshazehra72@gmail.com>
2024-09-26 01:55:29 -0500

    NavBar completed

--- my-pp/src/App.js
+++ my-pp/src/App.js
@@ -1,10 +1,10 @@
-import Navbar from "./Components/Navbar";
+import Navbar1 from "./Components/Navbar";  
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "../src/Styles/Colors.css"
 function App() {
   return (
     <div className="App">
-   <Navbar/>
+   <Navbar1/>
     </div>
   );
 }
--- my-pp/src/Components/Navbar.jsx
+++ my-pp/src/Components/Navbar.jsx
@@ -1,30 +1,79 @@
-import React, { useState } from 'react';
-import '../Styles/Navbar.css'; // Import the CSS file for styling
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "../Styles/Navbar.css";
+import logo from "../Assets/logo.png";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { IoSearchOutline } from "react-icons/io5";
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Toggle the menu open/close on mobile view
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen); // Toggle between true and false
-  };
-
+function Navbar1() {
+  const [showDropdown, setShowDropdown] = useState(null);
   return (
-    <nav className="navbar">
-      <div className="container">
-        <a className="brand" href="#">BrandName</a>
-        <button className="toggle-button" onClick={toggleNavbar}>
-          {isOpen ? '✖' : '☰'} {/* Display cross if menu is open, hamburger if closed */}
-        </button>
-        <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-          <a href="#" onClick={toggleNavbar}>Home</a>
-          <a href="#" onClick={toggleNavbar}>About</a>
-          <a href="#" onClick={toggleNavbar}>Services</a>
-          <a href="#" onClick={toggleNavbar}>Contact</a>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="nav-bg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="logo" className="nav-logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto gap-3 ">
+            <Nav.Link href="#home" className="nav-text fs-6">
+              Welcome
+            </Nav.Link>
+            <NavDropdown
+              href="#link"
+              className="nav-text"
+              title="Stores"
+              id="basic-nav-dropdown"
+              show={showDropdown === "stores"}
+              onMouseEnter={() => setShowDropdown("stores")}
+              onMouseLeave={() => setShowDropdown(null)}
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown
+              title="Categories"
+              id="basic-nav-dropdown"
+              className="nav-text"
+              show={showDropdown === "categories"}
+              onMouseEnter={() => setShowDropdown("categories")}
+              onMouseLeave={() => setShowDropdown(null)}
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline className="ms-auto">
+            <div className="position-relative me-auto">
+              <Form.Control
+                type="text"
+                placeholder="Search for stores"
+                className=" mr-sm-2"
+              />
+              <IoSearchOutline className="position-absolute" style={{right:"20px", top:"50%", transform: "translateY(-50%)", fontWeight:"800", fontSize:"20px"}} />
+            </div>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
-export default Navbar;
+export default Navbar1;
